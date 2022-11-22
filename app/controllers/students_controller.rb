d4a9b6801bd485890483562b73d2f9c3f41e0245
fliblip <wickynjy@gmail.com>
2022-11-22 12:06:08 +0300

    bdo bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def get_name
+    student = Student.find(params[:name])
+    render json: student
+  end
+
 end
